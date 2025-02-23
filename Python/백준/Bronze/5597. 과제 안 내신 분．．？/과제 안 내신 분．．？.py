df6fea1655b58e3bb325bddd007f20b77a39317f
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-02-23 23:18:10 +0900

    Update 과제 안 내신 분．．？.py

--- Python/백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ Python/백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,19 +1,19 @@
-stu_list = []
-cpr_list = []
-bad_list = []
-for i in range(28):
-    stu_list.append(int(input()))
+all_list = []
+suc_list = []
+fail_list = []
+
+#모든 학생 리스트
 for i in range(1,31):
-    cpr_list.append(i)
-bad_list = set(cpr_list) - set(stu_list)
-a,b = bad_list
+    all_list.append(i)
+#과제를 제출한 학생 리스트
+for i in range(28):
+    suc_list.append(int(input()))
+#과제를 내지 않은 학생 찾기
+fail_list = list(set(all_list) - set(suc_list))
 
-def max_value(a,b):
-    return a if a > b else b
-def min_value(a,b):
-    return a if a < b else b
-
-print(min_value(a,b))
-print(max_value(a,b))
-
+#출석 번호 순으로 정렬하기
+fail_list.sort()
+#출석 번호 순으로 출력하기
+print(fail_list[0])
+print(fail_list[1])
     
