11d29c5159ccfe53419955018255f57fe5b4a28c
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-02-28 01:06:05 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ Python/백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,17 +1,8 @@
-arr = [1,1,2,2,2,8] #있어야 하는 개수
-arr1 = list(map(int,input().split())) #가지고 있는 개수
+chess = [1,1,2,2,2,8] #있어야 하는 개수
+arr = list(map(int,input().split())) #가지고 있는 개수
 
-#피스 완성을 위한 개수
-arr2 = []
-for _ in range(len(arr1)):
-    arr2.append(0)
-    
-for i in range(len(arr1)):
-    if arr1[i] > arr[i]:
-        arr2[i] = arr[i]-arr1[i]
-    elif arr1[i] < arr[i]:
-        arr2[i] = arr[i] - arr1[i] #있어야하는 개수 - 가지고 있는 개수
-print(*arr2)        
+for i in range(len(arr)):
+    print(chess[i] - arr[i],end=" ")
 
 
         
