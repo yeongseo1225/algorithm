d98bebbd49896d888619a004d1592112bf80c78e
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-02-27 23:34:32 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,15 @@
+N, M = map(int,input().split())
+#N 바구니 개수
+#M 공을 바꿀 횟수
+#처음부터 바구니에 적혀있는 번호와 같은 번호가 적힌 공이 들어있음
+# 공을 바꿀 바구니 2개를 선택, 두 바구니에 들어있는 공을 교환
+arr = []
+sav = 0
+for i in range(N):
+    arr.append(i+1)
+for _ in range(M):
+    i,j = map(int,input().split())
+    sav = arr[i-1]
+    arr[i-1] = arr[j-1]
+    arr[j-1] = sav
+print(*arr)   
