166bae5cf6ac99075befe30c94b67528642bdd13
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-03-01 16:40:55 +0900

    [Bronze I] Title: 평균, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1546. 평균/평균.py
+++ Python/백준/Bronze/1546. 평균/평균.py
@@ -1,8 +1,5 @@
-N = int(input()) #시험 본 과목의 개수
-score = list(map(int,input().split())) #각 과목의 성적
+N = int(input())
+score = list(map(int,input().split()))
 M = max(score)
-sum = 0
-for i in score:
-    sum += i/M*100
-print(sum/N)    
- 
+sum = sum(score)
+print((sum/M*100)/N)
