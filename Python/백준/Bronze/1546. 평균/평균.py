24073f4e628522e38b9114e44be4830f5816268f
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-03-01 16:35:16 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1546. 평균/평균.py
+++ Python/백준/Bronze/1546. 평균/평균.py
@@ -1,9 +1,8 @@
-N = int(input()) #시험 본 과목 갯수
-scr_list = list(map(int,input().split())) #과목 당 점수 
-max_value = max(scr_list) #최고점
+N = int(input()) #시험 본 과목의 개수
+score = list(map(int,input().split())) #각 과목의 성적
+M = max(score)
 sum = 0
-for i in range(N):
-    scr_list[i] = scr_list[i]/max_value*100 # 점수/최댓값*100
-    sum = sum + scr_list[i]
-fake_avg = sum/N
-print(fake_avg)
+for i in score:
+    sum += i/M*100
+print(sum/N)    
+ 
