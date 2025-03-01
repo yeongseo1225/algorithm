0e6318aec706c4ff370745a3170cbea9b6b06d80
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-03-01 16:19:01 +0900

    [Bronze IV] Title: 숫자의 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ Python/백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,6 +1,6 @@
-N = int(input()) #숫자의 갯수
-S = input() #문자열로 입력 받기
+N = int(input())
+S = input()
 sum = 0
 for i in range(N):
-    sum = sum + int(S[i]) #문자열 정수형으로 변환
-print(sum)
+    sum += int(S[i])
+print(sum)    
