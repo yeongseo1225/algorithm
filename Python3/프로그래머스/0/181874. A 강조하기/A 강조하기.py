bc7529f273453d15ea572c3cd7a3ec71037d527a
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-03-08 22:45:40 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ Python3/프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,10 +1,3 @@
 def solution(myString):
-    answer = ''
-    for i in myString:
-        if i == "a":
-            answer += i.upper()
-        elif i != "A":
-            answer += i.lower()
-        else:
-            answer += i
+    answer = myString.lower().replace("a","A")
     return answer
