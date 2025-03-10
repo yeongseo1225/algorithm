bc12747d28b23b6b304cbcfcb96288b83726f6fe
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-03-10 12:10:06 +0900

    [level 1] Title: 음양 더하기, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = 0
+    for absolute, sign in zip(absolutes, signs):
+        if sign:
+            answer += absolute
+        else:
+            answer -= absolute
+            
+    return answer
