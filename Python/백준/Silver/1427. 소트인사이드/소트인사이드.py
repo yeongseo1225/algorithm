689d943d2eba4fed3ed4844390acd750212f9dc9
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-11-19 17:22:18 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ Python/백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+
+A = list(input())
+
+for i in range(len(A)):
+    Max = i
+    for j in range(i+1,len(A)):
+        if A[j] > A[Max]:
+            Max = j
+    if A[i] < A[Max]:
+        temp = A[i]
+        A[i] = A[Max]
+        A[Max] = temp
+        
+for i in range(len(A)):
+    print(A[i])
